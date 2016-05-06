1b8875c557ba4158104937f447419339aa7ad130
Licshee <licshee@yen.ai>
2016-05-07 05:17:51 +0800

    v0.0

---
+++ As/CastAsUtility.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Linq.Expressions;
+
+using static System.Linq.Expressions.Expression;
+
+public static class CastAsUtility
+{
+    static readonly Type TypeObject = typeof(object);
+    static readonly Type TypeNullable = typeof(Nullable<>);
+
+    static readonly ParameterExpression Param0 = Parameter(TypeObject);
+
+    static class CastImpl<T>
+    {
+        public class Item
+        {
+            public Lazy<Func<object, T>> As;
+            public Lazy<Expression<Func<object, T>>> AsExpr;
+            public Lazy<Func<object, T>> Cast;
+            public Lazy<Expression<Func<object, T>>> CastExpr;
+        }
+
+        static readonly ConcurrentDictionary<Type, Lazy<Item>> Dict = new ConcurrentDictionary<Type, Lazy<Item>>();
+
+        public static readonly Type TypeT = typeof(T);
+        public static readonly bool? IsNullable;
+
+        static readonly Expression<Func<object, T>> NullExpr;
+        static readonly Func<object, T> NullFunc;
+
+        static CastImpl()
+        {
+            if (TypeT.IsGenericTypeDefinition)
+                throw new NotSupportedException();
+
+            if (!TypeT.IsValueType)
+                IsNullable = null;
+            else if (!TypeT.IsGenericType)
+                IsNullable = false;
+            else
+            {
+                IsNullable = (TypeT.GetGenericTypeDefinition() == TypeNullable);
+            }
+
+            if (IsNullable.GetValueOrDefault(true))
+            {
+                NullExpr = Lambda<Func<object, T>>(Constant(null, TypeT), Param0);
+                NullFunc = NullExpr.Compile();
+            }
+        }
+
+        public static Item For(Type type)
+        => Dict.GetOrAdd(type, new Lazy<Item>(() =>
+        {
+            var item = new Item();
+
+            if (!IsNullable.GetValueOrDefault())
+                item.CastExpr = new Lazy<Expression<Func<object, T>>>(() => Lambda<Func<object, T>>(Convert(Convert(Param0, type), TypeT), Param0));
+            else // T is Nullable<?>
+                item.CastExpr = new Lazy<Expression<Func<object, T>>>(() =>
+                    Lambda<Func<object, T>>(Convert(Convert(Convert(Param0, type), TypeT.GetGenericArguments().Single()), TypeT), Param0));
+            item.Cast = new Lazy<Func<object, T>>(() => item.CastExpr.Value.Compile());
+
+            // T can not be null
+            if (!IsNullable.GetValueOrDefault(true))
+                return item;
+
+            item.AsExpr = new Lazy<Expression<Func<object, T>>>(() =>
+            {
+                try
+                {
+                    return item.CastExpr.Value;
+                }
+                catch
+                {
+                    return NullExpr;
+                }
+            });
+            item.As = new Lazy<Func<object, T>>(() =>
+            {
+                try
+                {
+                    return item.Cast.Value;
+                }
+                catch
+                {
+                    return NullFunc;
+                }
+            });
+
+            return item;
+        })).Value;
+
+        public static T TypeCast(object o, Type type)
+            => For(type).Cast.Value(o);
+    }
+
+    static class AsProxy<T>
+    {
+        static AsProxy()
+        {
+            if (!CastImpl<T>.IsNullable.GetValueOrDefault(true))
+                throw new NotSupportedException();
+        }
+
+        public static T TypeAs(object o, Type type)
+            => CastImpl<T>.For(type).As.Value(o);
+        public static Expression<Func<object, T>> GetAsExprFor(Type type)
+            => CastImpl<T>.For(type).AsExpr.Value;
+    }
+
+    public static T TypeAs<T>(this object o, Type type)
+        => AsProxy<T>.TypeAs(o, type);
+
+    public static T TypeCast<T>(this object o, Type type)
+        => CastImpl<T>.TypeCast(o, type);
+
+    public static T As<T>(this object o)
+        => (o == null) ? default(T) : AsProxy<T>.TypeAs(o, o.GetType());
+
+    public static T Cast<T>(this object o)
+        => (o == null) ? default(T) : CastImpl<T>.TypeCast(o, o.GetType());
+
+    public static Expression<Func<object, T>> GetAsExprFor<T>(this Type type)
+        => AsProxy<T>.GetAsExprFor(type);
+    public static Expression<Func<object, T>> GetCastExprFor<T>(this Type type)
+        => CastImpl<T>.For(type).CastExpr.Value;
+}
---
+++ AsTests/AsTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using Xunit;
+
+public class AsTests
+{
+    class A
+    {
+        public static explicit operator string(A self)
+            => self?.ToString();
+    }
+
+    class B
+    {
+        public static implicit operator B(string s)
+            => new B();
+    }
+
+    [Fact]
+    public void Cast()
+    {
+        var sw = Stopwatch.StartNew();
+
+        CastTestCore();
+        Trace.WriteLine(sw.ElapsedMilliseconds);
+        sw.Restart();
+
+        CastTestCore();
+        Trace.WriteLine(sw.ElapsedMilliseconds);
+    }
+
+    void CastTestCore()
+    {
+        Assert.Equal(null, ((object)null).Cast<object>());
+
+        Assert.Equal(1d, 1.Cast<double>());
+        Assert.Equal(1d, 1.Cast<double?>());
+        Assert.Equal(1, 1.Cast<object>());
+        Assert.ThrowsAny<Exception>(() => 1.Cast<string>());
+        Assert.ThrowsAny<Exception>(() => 1.As<double>());
+        Assert.Equal(1d, 1.As<double?>());
+        Assert.Equal(1, 1.As<object>());
+        Assert.Equal(null, 1.As<string>());
+
+        Assert.Equal("", "".Cast<object>());
+        Assert.Equal("", "".Cast<string>());
+        Assert.ThrowsAny<Exception>(() => "".Cast<Guid>());
+        Assert.ThrowsAny<Exception>(() => "".As<double>());
+        Assert.Equal("", "".As<object>());
+        Assert.Equal("", "".As<string>());
+
+        var a = new A();
+        var s = a.ToString();
+        Assert.Equal(a, a.Cast<A>());
+        Assert.Equal(a, a.Cast<object>());
+        Assert.Equal(s, a.Cast<string>());
+        Assert.Equal(a, a.As<A>());
+        Assert.Equal(a, a.As<object>());
+        Assert.Equal(s, a.As<string>());
+
+        Assert.Equal(null, ((int?)null).As<B>());
+        Assert.ThrowsAny<Exception>(() => a.Cast<B>());
+        Assert.Equal(null, a.As<B>());
+        Assert.IsType<B>(s.As<B>());
+        Assert.IsType<B>(s.Cast<B>());
+        Assert.IsType<B>(((int?)null).TypeAs<B>(typeof(string)));
+        Assert.IsType<B>(((int?)null).TypeCast<B>(typeof(string)));
+    }
+}
