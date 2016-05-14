99bf59c314c503db011379457974f10fbdb2b326
Licshee <licshee@yen.ai>
2016-05-14 15:39:54 +0800

    suppose to fix #2

--- As/CastAsUtility.cs
+++ As/CastAsUtility.cs
@@ -63,19 +63,19 @@ public static class CastAsUtility
         public class Item
         {
             public Lazy<Func<object, T>> UAs;
-            public Lazy<Expression<Func<object, T>>> UAsExpr;
+            public Lazy<Expression<Func<object, T>>> UAsLambda;
             public Lazy<Func<object, T>> UTo;
-            public Lazy<Expression<Func<object, T>>> UToExpr;
+            public Lazy<Expression<Func<object, T>>> UToLambda;
 
             public Lazy<Func<object, T>> CAs;
-            public Lazy<Expression<Func<object, T>>> CAsExpr;
+            public Lazy<Expression<Func<object, T>>> CAsLambda;
             public Lazy<Func<object, T>> CTo;
-            public Lazy<Expression<Func<object, T>>> CToExpr;
+            public Lazy<Expression<Func<object, T>>> CToLambda;
         }
 
         static readonly ConcurrentDictionary<Type, Lazy<Item>> Dict = new ConcurrentDictionary<Type, Lazy<Item>>();
 
-        static readonly Expression<Func<object, T>> NullExpr;
+        static readonly Expression<Func<object, T>> NullLambda;
         static readonly Func<object, T> NullFunc;
 
         static CastImpl()
@@ -95,46 +95,102 @@ public static class CastAsUtility
 
             if (IsNullable.GetValueOrDefault(true))
             {
-                NullExpr = Lambda<Func<object, T>>(Constant(null, ThisType), Param0);
-                NullFunc = NullExpr.Compile();
+                NullLambda = Lambda<Func<object, T>>(Constant(null, ThisType), Param0);
+                NullFunc = NullLambda.Compile();
             }
         }
 
+        static UnaryExpression ConvertFactory(Expression expr, Type type, bool @checked)
+            => @checked ? ConvertChecked(expr, type) : Convert(expr, type);
+        static Expression<Func<object, T>> LambdaFactory(Expression expr, Type type, bool @checked)
+        {
+            var nonNullableType = GetNonNullable(type);
+
+            if (IsNullable.HasValue)
+            {
+                if (IsNullable.Value)
+                {
+                    if (nonNullableType != null)
+                        throw new NotSupportedException();
+
+                    try
+                    {
+                        expr = ConvertFactory(expr, ThisType, @checked);
+                    }
+                    catch
+                    {
+                        expr = ConvertFactory(expr, TypeNonNullable, @checked);
+                        expr = Convert(expr, ThisType);
+                    }
+                }
+                else
+                {
+                    if (nonNullableType == null)
+                        expr = ConvertFactory(expr, ThisType, @checked);
+                    else
+                    {
+                        try
+                        {
+                            expr = ConvertFactory(expr, ThisType, @checked);
+                        }
+                        catch
+                        {
+                            expr = ConvertFactory(expr, nonNullableType, @checked);
+                            expr = Convert(expr, ThisType);
+                        }
+                    }
+                }
+
+                return Lambda<Func<object, T>>(expr, Param0);
+            }
+
+            try
+            {
+                if (nonNullableType == null)
+                    expr = Convert(expr, ThisType);
+                else
+                {
+                    try
+                    {
+                        expr = Convert(expr, ThisType);
+                    }
+                    catch
+                    {
+                        expr = Convert(expr, nonNullableType);
+                        expr = Convert(expr, ThisType);
+                    }
+                }
+
+                return Lambda<Func<object, T>>(expr, Param0);
+            }
+            catch
+            {
+                type = type.BaseType;
+                if (type.IsAssignableFrom(ThisType))
+                    throw;
+                return For(type).CToLambda.Value;
+            }
+        }
         public static Item For(Type type)
         => Dict.GetOrAdd(type, new Lazy<Item>(() =>
         {
             var expr0 = Convert(Param0, type);
             var item = new Item();
 
-            item.UToExpr = new Lazy<Expression<Func<object, T>>>(() =>
-            {
-                try
-                {
-                    var expr = !IsNullable.GetValueOrDefault() ? Convert(expr0, ThisType)
-                         : Convert(Convert(expr0, TypeNonNullable), ThisType);
-                    return Lambda<Func<object, T>>(expr, Param0);
-                }
-                catch
-                {
-                    type = type.BaseType;
-                    if (type.IsAssignableFrom(ThisType))
-                        throw;
-                    return For(type).UToExpr.Value;
-                }
-            });
-            item.UTo = new Lazy<Func<object, T>>(() => item.UToExpr.Value.Compile());
+            item.UToLambda = new Lazy<Expression<Func<object, T>>>(() => LambdaFactory(expr0, type, false));
+            item.UTo = new Lazy<Func<object, T>>(() => item.UToLambda.Value.Compile());
 
             if (IsNullable.GetValueOrDefault(true))
             {
-                item.UAsExpr = new Lazy<Expression<Func<object, T>>>(() =>
+                item.UAsLambda = new Lazy<Expression<Func<object, T>>>(() =>
                 {
                     try
                     {
-                        return item.UToExpr.Value;
+                        return item.UToLambda.Value;
                     }
                     catch
                     {
-                        return NullExpr;
+                        return NullLambda;
                     }
                 });
                 item.UAs = new Lazy<Func<object, T>>(() =>
@@ -153,43 +209,28 @@ public static class CastAsUtility
             var useChecked = IsPrimitiveIntegral.GetValueOrDefault() && IsPrimitiveIntegral(type).HasValue;
             if (!useChecked)
             {
-                item.CToExpr = item.UToExpr;
+                item.CToLambda = item.UToLambda;
                 item.CTo = item.UTo;
-                item.CAsExpr = item.UAsExpr;
+                item.CAsLambda = item.UAsLambda;
                 item.CAs = item.UAs;
 
                 return item;
             }
 
-            item.CToExpr = new Lazy<Expression<Func<object, T>>>(() =>
-            {
-                try
-                {
-                    var expr = !IsNullable.GetValueOrDefault() ? ConvertChecked(expr0, ThisType)
-                         : Convert(ConvertChecked(expr0, TypeNonNullable), ThisType);
-                    return Lambda<Func<object, T>>(expr, Param0);
-                }
-                catch
-                {
-                    type = type.BaseType;
-                    if (type.IsAssignableFrom(ThisType))
-                        throw;
-                    return For(type).CToExpr.Value;
-                }
-            });
-            item.CTo = new Lazy<Func<object, T>>(() => item.CToExpr.Value.Compile());
+            item.CToLambda = new Lazy<Expression<Func<object, T>>>(() => LambdaFactory(expr0, type, true));
+            item.CTo = new Lazy<Func<object, T>>(() => item.CToLambda.Value.Compile());
 
             if (IsNullable.GetValueOrDefault(true))
             {
-                item.CAsExpr = new Lazy<Expression<Func<object, T>>>(() =>
+                item.CAsLambda = new Lazy<Expression<Func<object, T>>>(() =>
                 {
                     try
                     {
-                        return item.CToExpr.Value;
+                        return item.CToLambda.Value;
                     }
                     catch
                     {
-                        return NullExpr;
+                        return NullLambda;
                     }
                 });
                 item.CAs = new Lazy<Func<object, T>>(() =>
@@ -216,6 +257,8 @@ public static class CastAsUtility
 
     static class AsProxy<T>
     {
+        public static readonly T Default;
+
         static AsProxy()
         {
             if (!CastImpl<T>.IsNullable.GetValueOrDefault(true))
@@ -226,10 +269,10 @@ public static class CastAsUtility
             => CastImpl<T>.For(type).UAs.Value(o);
         public static T AsChecked(object o, Type type)
             => CastImpl<T>.For(type).CAs.Value(o);
-        public static Expression<Func<object, T>> GetAsExprFor(Type type)
-            => CastImpl<T>.For(type).UAsExpr.Value;
-        public static Expression<Func<object, T>> GetAsExprForChecked(Type type)
-            => CastImpl<T>.For(type).CAsExpr.Value;
+        public static Expression<Func<object, T>> GetAsLambdaFor(Type type)
+            => CastImpl<T>.For(type).UAsLambda.Value;
+        public static Expression<Func<object, T>> GetAsLambdaForChecked(Type type)
+            => CastImpl<T>.For(type).CAsLambda.Value;
     }
 
     public static T As<T>(this object o, Type type)
@@ -243,22 +286,22 @@ public static class CastAsUtility
         => CastImpl<T>.ToChecked(o, type);
 
     public static T As<T>(this object o)
-        => (o == null) ? default(T) : AsProxy<T>.As(o, o.GetType());
+        => (o == null) ? AsProxy<T>.Default : AsProxy<T>.As(o, o.GetType());
     public static T AsChecked<T>(this object o)
-        => (o == null) ? default(T) : AsProxy<T>.AsChecked(o, o.GetType());
+        => (o == null) ? AsProxy<T>.Default : AsProxy<T>.AsChecked(o, o.GetType());
 
     public static T To<T>(this object o)
-        => (o == null) ? default(T) : CastImpl<T>.To(o, o.GetType());
+        => (o == null) ? AsProxy<T>.Default : CastImpl<T>.To(o, o.GetType());
     public static T ToChecked<T>(this object o)
-        => (o == null) ? default(T) : CastImpl<T>.ToChecked(o, o.GetType());
+        => (o == null) ? AsProxy<T>.Default : CastImpl<T>.ToChecked(o, o.GetType());
 
-    public static Expression<Func<object, T>> GetAsExprFor<T>(this Type type)
-        => AsProxy<T>.GetAsExprFor(type);
-    public static Expression<Func<object, T>> GetAsExprForChecked<T>(this Type type)
-        => AsProxy<T>.GetAsExprForChecked(type);
+    public static Expression<Func<object, T>> GetAsLambdaFor<T>(this Type type)
+        => AsProxy<T>.GetAsLambdaFor(type);
+    public static Expression<Func<object, T>> GetAsLambdaForChecked<T>(this Type type)
+        => AsProxy<T>.GetAsLambdaForChecked(type);
 
-    public static Expression<Func<object, T>> GetToExprFor<T>(this Type type)
-        => CastImpl<T>.For(type).UToExpr.Value;
-    public static Expression<Func<object, T>> GetToExprForChecked<T>(this Type type)
-        => CastImpl<T>.For(type).CToExpr.Value;
+    public static Expression<Func<object, T>> GetToLambdaFor<T>(this Type type)
+        => CastImpl<T>.For(type).UToLambda.Value;
+    public static Expression<Func<object, T>> GetToLambdaForChecked<T>(this Type type)
+        => CastImpl<T>.For(type).CToLambda.Value;
 }
