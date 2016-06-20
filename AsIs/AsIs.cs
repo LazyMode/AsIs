43d6998796f9b7f49c3ccd75e3d4fdfc62601617
Licshee <licshee@yen.ai>
2016-06-20 11:31:25 +0800

    Coalesce

--- AsIs/AsIs.cs
+++ AsIs/AsIs.cs
@@ -11,6 +11,16 @@ using TypeInfo = System.Type;
 
 public static class AsIs
 {
+    public static T Coalesce<T>(this T self, Action action)
+    {
+        action();
+        return self;
+    }
+    public static T Coalesce<T>(this T self, Action<T> action)
+    {
+        action(self);
+        return self;
+    }
     public static T Run<T>(Func<T> func) => func();
     public static void Run(Action logic) => logic();
 
